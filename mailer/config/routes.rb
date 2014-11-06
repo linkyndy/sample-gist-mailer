b3cbd2c166ded01e614c8d04968475f98bffc72e
Andrei Horak <linkyndy@gmail.com>
2014-11-06 13:42:26 +0200

    Fixed routes and added a view

--- mailer/config/routes.rb
+++ mailer/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'gist_mail#new'
 
-  post 'confirm' => 'gist_mail#confirm'
-  post 'send' => 'gist_mail#send'
+  post '/confirm' => 'gist_mail#confirm'
+  post '/send' => 'gist_mail#send'
 end
