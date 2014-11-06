2b82bec395aa076c99520c3449ef68d52d2ffe64
Andrei Horak <linkyndy@gmail.com>
2014-11-06 10:35:36 +0200

    Fixed model validations

--- mailer/app/models/mail.rb
+++ mailer/app/models/mail.rb
@@ -3,13 +3,10 @@ class Mail
 
 	attr_accessor :name, :email, :confirm_email, :gist_url, :country, :city
 
-	validates :name, :presence => true
-	validates :email, :with => /@/, :presence => true
-	validates :confirm_email, :if => :fields_similar?(:confirm_email, :email)
-	validates :gist_url, :with => URI::regexp(%w(http https)), :presence => true
-	validates :country, :presence => true
-	validates :city, :presence => true
-
-	def fields_similar?(who, whom)
-		who == whom
-	end
+	validates :name, presence: true
+	validates :email, with: /@/, confirmation: true, presence: true
+	validates :email_confirmation, presence: true
+	validates :gist_url, with: URI::regexp(%w(http https)), presence: true
+	validates :country, presence: true
+	validates :city, presence: true
+end
