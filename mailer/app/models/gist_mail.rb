7b93b407837dda9243748df6b82f471c7830f5c4
Andrei Horak <linkyndy@gmail.com>
2014-11-07 18:35:05 +0200

    Completed model line-up
    Added Country and City model and implemented validation on GistMail model

--- mailer/app/models/gist_mail.rb
+++ mailer/app/models/gist_mail.rb
@@ -1,12 +1,22 @@
-class GistMail
-	include ActiveModel::Validations
+class GistMail < ActiveRecord::Base
+	has_no_table
 
-	attr_accessor :name, :email, :email_confirmation, :gist_url, :country, :city
+	# attr_accessor :name, :email, :email_confirmation, :gist_url, :country, :city
+	column :name, :string
+	column :email, :string
+	column :gist_url, :string
+	column :country_name, :string
+	column :city_name, :string
 
 	validates :name, presence: true
 	validates :email, with: /@/, confirmation: true, presence: true
 	validates :email_confirmation, presence: true
 	validates :gist_url, with: URI::regexp(%w(http https)), presence: true
-	validates :country, presence: true
-	validates :city, presence: true
+	validates :country_name, presence: true
+	validates :city_name, presence: true
+
+	validate do
+		errors.add(:country, 'Invalid country') unless Country.exists?(name: self.country_name)
+		errors.add(:city, 'Invalid city') unless City.where(name: self.city_name).first.country.name == self.country_name
+	end
 end
